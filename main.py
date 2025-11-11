37e1129e4f9607e919d8482dae29374919f5f33d
arkksu <115230995+arkksu@users.noreply.github.com>
2025-11-11 15:33:13 +0300

    document routes created

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Header
 from models import *
-
+from database import *
 from cache_manager import *
 from permissions_manager import *
+
+from datetime import datetime
 
 app = FastAPI()
 
@@ -17,3 +19,39 @@ async def login(data: LoginModel):
 async def clear_cache():
     # TODO: Implement cache clearing logic
     return {"message": "Cache cleared"}
+
+
+@app.post("/documents", response_model=DocumentOut, status_code=status.HTTP_201_CREATED)
+async def create_document(document: DocumentCreate, x_user_id: int = Header(...)):
+    new_id = max(fake_documents_db.keys(), default=0) + 1
+    new_iso = datetime.now().isoformat()
+
+    document_response = DocumentOut(
+        id=new_id,
+        title=document.title,
+        content=document.content,
+        category=document.category,
+        is_public=document.is_public,
+        created_by=x_user_id,
+        created_at=new_iso,
+        last_modified=new_iso
+    )
+
+    # Database.set_document(new_id, document_response) TODO: ...
+
+    return document_response
+
+@app.put("/documents/{doc_id}")
+async def update_document(document: DocumentUpdate, doc_id: int):
+    # TODO: ...
+    return {"message": f"Document {doc_id} updated"}
+
+@app.post("/documents/{doc_id}/share")
+async def share_document(doc_id: int):
+    # TODO: ...
+    return {"message": f"Document {doc_id} shared"}
+
+@app.delete("/documents/{doc_id}/share")
+async def delete_document_share(doc_id: int):
+    # TODO: ...
+    return {"message": f"Share for document {doc_id} deleted"}
